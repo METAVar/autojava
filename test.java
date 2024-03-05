f5386e3108b04959a7dfa4c8d7985da4e278c4b8
Abhijeet Vaidya <abhijeetavaidya@gmail.com>
2024-03-05 23:36:21 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,28 @@
 import java.util.Random;
 
+
+public class RandomFirstNameGenerator {
+    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.";
+    private static final int MIN_LENGTH = 8;
+    private static final int MAX_LENGTH = 50;
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+        String firstName = generateRandomFirstName();
+        System.out.println(firstName);
+    }
+
+    private static String generateRandomFirstName() {
+        int length = MIN_LENGTH + random.nextInt(MAX_LENGTH - MIN_LENGTH + 1);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+}
+
+
 public class RandomEmailGenerator {
     private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
     private static final String UPPERCASE = LOWERCASE.toUpperCase();
